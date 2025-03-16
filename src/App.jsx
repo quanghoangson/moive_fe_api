682d9d3067da13ea51a6cbef4da8793139ded7b2
quanghoangson <quanghoangson2.3@gmail.com>
2025-03-17 05:51:10 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+function App() {
+  return (
+    <>
+      <div className="">
+        <Header />
+        <Banner />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extends: {
+      backgroundImage: {
+        banner: "url(/banner.png)",
+      },
+    },
+    plugin: [],
+  },
+};
